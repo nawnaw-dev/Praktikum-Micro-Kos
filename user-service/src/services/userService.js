d33493d9eed24fd452bf69edc69609c2c82ac779
ShaviraNindya5 <shaviranindya5@gmail.com>
2026-04-15 01:20:50 +0700

    fix login user service

--- user-service/src/services/userService.js
+++ user-service/src/services/userService.js
@@ -1,5 +1,6 @@
 let users = [];
 
+// CREATE USER
 exports.createUser = (data) => {
   const newUser = {
     id: users.length + 1,
@@ -12,32 +13,72 @@ exports.createUser = (data) => {
   return newUser;
 };
 
+// LOGIN USER (FIXED + JELAS RESPONSE)
 exports.login = (data) => {
-  return users.find(
+  const user = users.find(
     (u) => u.email === data.email && u.password === data.password
   );
+
+  if (!user) {
+    return {
+      success: false,
+      message: "Login gagal: email atau password salah",
+    };
+  }
+
+  return {
+    success: true,
+    message: "Login berhasil",
+    data: user,
+  };
 };
 
-exports.getUsers = () => users;
+// GET ALL USERS
+exports.getUsers = () => {
+  return users;
+};
 
+// GET BY ID
 exports.getUserById = (id) => {
   return users.find((u) => u.id === id);
 };
 
+// UPDATE USER
 exports.updateUser = (id, data) => {
   const user = users.find((u) => u.id === id);
-  if (!user) return null;
+  if (!user) {
+    return {
+      success: false,
+      message: "User tidak ditemukan",
+    };
+  }
 
   user.name = data.name || user.name;
   user.email = data.email || user.email;
+  user.password = data.password || user.password;
 
-  return user;
+  return {
+    success: true,
+    message: "User berhasil diupdate",
+    data: user,
+  };
 };
 
+// DELETE USER
 exports.deleteUser = (id) => {
   const index = users.findIndex((u) => u.id === id);
-  if (index === -1) return false;
+
+  if (index === -1) {
+    return {
+      success: false,
+      message: "User tidak ditemukan",
+    };
+  }
 
   users.splice(index, 1);
-  return true;
+
+  return {
+    success: true,
+    message: "User berhasil dihapus",
+  };
 };
